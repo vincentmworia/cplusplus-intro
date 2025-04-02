afe71ab60835a72262d51a5f29efdf92fc8aa382
Vincent Mwenda Mworia <mworiavince@gmail.com>
2025-04-02 19:26:15 +0200

    first program done

---
+++ Section4/1FirstProgram/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    
+    int favorite_number;
+    std::cout<< "Hello, what is your favorite number?";
+    std::cin >> favorite_number;
+    std::cout<< "Amazing!! That is my favorite number too!" << std::endl;
+    return 0;
+}
